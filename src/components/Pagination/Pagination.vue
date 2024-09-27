057bd4f30ecee278a2242b6a8798aa242b02db99
reviwh <reviwh2@gmail.com>
2024-09-27 18:21:50 +0700

    feat(component): add pagination

--- src/components/Pagination/Pagination.vue
+++ src/components/Pagination/Pagination.vue
@@ -1,50 +1,76 @@
 <template>
   <div class="pagination">
     <div class="page-controller">
-      <div :class="{ disabled: selectedPage === 1 }"></div>
-      <div :class="{ disabled: selectedPage === 1 }"></div>
-      <!-- Case 1: First 5 pages -->
-      <template v-if="selectedPage <= 3">
-        <div v-for="i in 5" :key="i" :class="{ selected: selectedPage === i }">
-          {{ i }}
+      <template v-if="lastPage > 5">
+        <div :class="{ disabled: selectedPage === 1 }">
+          <icon name="double-arrow-backward" />
+        </div>
+        <div :class="{ disabled: selectedPage === 1 }">
+          <icon name="arrow-back" />
+        </div>
+        <!-- Case 1: First 5 pages -->
+        <template v-if="selectedPage <= 3">
+          <div
+            v-for="i in 5"
+            :key="i"
+            :class="[{ selected: selectedPage === i }, 'text-title-sm']"
+          >
+            {{ i }}
+          </div>
+        </template>
+
+        <!-- Case 2: Last 5 pages -->
+        <template v-else-if="selectedPage > lastPage - 3">
+          <div
+            v-for="i in Array.from(
+              { length: 5 },
+              (_, index) => lastPage - 4 + index
+            )"
+            :key="i"
+            :class="{ selected: selectedPage === i }"
+          >
+            {{ i }}
+          </div>
+        </template>
+
+        <!-- Case 3: Middle pages -->
+        <template v-else>
+          <div
+            v-for="i in Array.from(
+              { length: 5 },
+              (_, index) => selectedPage - 2 + index
+            )"
+            :key="i"
+            :class="{ selected: selectedPage === i }"
+          >
+            {{ i }}
+          </div>
+        </template>
+        <div :class="{ disabled: selectedPage === lastPage }">
+          <icon name="arrow-forward" />
+        </div>
+        <div :class="{ disabled: selectedPage === lastPage }">
+          <icon name="double-arrow-forward" />
         </div>
       </template>
-
-      <!-- Case 2: Last 5 pages -->
-      <template v-else-if="selectedPage > lastPage - 3">
-        <div
-          v-for="i in Array.from(
-            { length: 5 },
-            (_, index) => lastPage - 4 + index
-          )"
-          :key="i"
-          :class="{ selected: selectedPage === i }"
-        >
-          {{ i }}
-        </div>
-      </template>
-
-      <!-- Case 3: Middle pages -->
       <template v-else>
         <div
-          v-for="i in Array.from(
-            { length: 5 },
-            (_, index) => selectedPage - 2 + index
-          )"
+          v-for="i in lastPage"
           :key="i"
-          :class="{ selected: selectedPage === i }"
+          :class="[{ selected: selectedPage === i }, 'text-title-sm']"
         >
           {{ i }}
         </div>
       </template>
-      <div :class="{ disabled: selectedPage === lastPage }"></div>
-      <div :class="{ disabled: selectedPage === lastPage }"></div>
     </div>
+    <p class="text-body-sm">{{ first }} - {{ last }} of {{ total }} items</p>
   </div>
 </template>
 
 <script lang="ts">
+import Icon from "../Icon.vue";
 export default {
+  components: { Icon },
   name: "Pagination",
   props: {
     selectedPage: {
@@ -81,7 +107,42 @@ export default {
 </script>
 
 <style>
-.selected {
+.pagination {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+}
+.page-controller {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+.page-controller div {
+  width: 2rem;
+  height: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  color: var(--on-surface-variant);
+}
+
+.page-controller div svg {
+  width: 1.25rem;
+  height: 1.25rem;
+}
+.page-controller .selected {
   background: var(--primary);
+  color: var(--on-primary) !important;
+}
+.page-controller .disabled {
+  opacity: 0.5;
+  cursor: default;
 }
 </style>
